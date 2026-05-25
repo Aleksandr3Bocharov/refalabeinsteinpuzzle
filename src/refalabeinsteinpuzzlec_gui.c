5c43bd6d79efa89e50b71d0caf1a82c45a43865a
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-05-25 21:36:38 +0300

    Add files via upload

--- src/refalabeinsteinpuzzlec_gui.c
+++ src/refalabeinsteinpuzzlec_gui.c
@@ -1,7 +1,7 @@
-// Copyright 2025 Aleksandr Bocharov
+// Copyright 2026 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2025-10-07
+// 2026-05-25
 // https://github.com/Aleksandr3Bocharov/refalabeinsteinpuzzle
 
 //====================================================================
@@ -31,7 +31,7 @@ static char table[6][6][32] = {{{"Дом"}, {"Цвет"}, {"Национальн
 // <GUI_Init> ==
 static void gui_init_(void)
 {
-    if (refal.preva->next != refal.nexta)
+    if (refal.previous_argument->next != refal.next_argument)
     {
         refal.upshot = 2;
         return;
@@ -46,7 +46,7 @@ void (*gui_init_1)(void) = gui_init_;
 // <GUI_Close> ==
 static void gui_close_(void)
 {
-    if (refal.preva->next != refal.nexta)
+    if (refal.previous_argument->next != refal.next_argument)
     {
         refal.upshot = 2;
         return;
@@ -61,16 +61,16 @@ void (*gui_close_1)(void) = gui_close_;
 // <Dialog_Info> == &True | &False
 static void dialog_info_(void)
 {
-    if (refal.preva->next != refal.nexta)
+    if (refal.previous_argument->next != refal.next_argument)
     {
         refal.upshot = 2;
         return;
     }
     if (dialog_Info())
-        refal.preva->info.codef = &refalab_true;
+        refal.previous_argument->info.codef = &refalab_true;
     else
-        refal.preva->info.codef = &refalab_false;
-    rftpl(refal.prevr, refal.nextr, refal.nexta);
+        refal.previous_argument->info.codef = &refalab_false;
+    transplantation(refal.previous_result, refal.previous_argument->previous, refal.previous_argument->next);
     return;
 }
 char dialog_info_0[] = {Z3 'D', 'I', 'A', 'L', 'O', 'G', '_', 'I', 'N', 'F', 'O', (char)11};
@@ -82,17 +82,17 @@ static void view_answer_(void)
 {
     do
     {
-        T_LINKCB *p = refal.preva->next;
-        if (p->tag != TAGN)
+        T_LINKCB *current_argument = refal.previous_argument->next;
+        if (current_argument->tag != TAGN)
             break;
-        steps = gcoden(p);
-        p = p->next;
-        if (p->tag != TAGN)
+        steps = gcoden(current_argument);
+        current_argument = current_argument->next;
+        if (current_argument->tag != TAGN)
             break;
-        steps_Back = gcoden(p);
-        p = p->next;
-        p = rfgstr(answer, 20, p);
-        if (p != refal.nexta)
+        steps_Back = gcoden(current_argument);
+        current_argument = current_argument->next;
+        current_argument = get_string_expression(answer, 20, current_argument, refal.next_argument);
+        if (current_argument != refal.next_argument)
             break;
         return;
     } while (false);
@@ -108,21 +108,21 @@ static void view_rule_(void)
 {
     do
     {
-        T_LINKCB *p = refal.preva->next;
-        if (p->tag != TAGN)
+        T_LINKCB *current_argument = refal.previous_argument->next;
+        if (current_argument->tag != TAGN)
             break;
-        position = gcoden(p);
+        position = gcoden(current_argument);
         if (position > 2)
             break;
-        p = p->next;
-        if (p->tag != TAGN)
+        current_argument = current_argument->next;
+        if (current_argument->tag != TAGN)
             break;
-        rule = gcoden(p);
+        rule = gcoden(current_argument);
         if (rule > 16)
             break;
-        p = p->next;
-        p = rfgstr(rule_Text, 149, p);
-        if (p != refal.nexta)
+        current_argument = current_argument->next;
+        current_argument = get_string_expression(rule_Text, 149, current_argument, refal.next_argument);
+        if (current_argument != refal.next_argument)
             break;
         return;
     } while (false);
@@ -138,21 +138,21 @@ static void view_table_(void)
 {
     do
     {
-        T_LINKCB *p = refal.preva->next;
-        if (p->tag != TAGN)
+        T_LINKCB *current_argument = refal.previous_argument->next;
+        if (current_argument->tag != TAGN)
             break;
-        const uint32_t row = gcoden(p);
+        const uint32_t row = gcoden(current_argument);
         if (row < 1 || row > 5)
             break;
-        p = p->next;
-        if (p->tag != TAGN)
+        current_argument = current_argument->next;
+        if (current_argument->tag != TAGN)
             break;
-        const uint32_t col = gcoden(p);
-        if (col > 5)
+        const uint32_t column = gcoden(current_argument);
+        if (column > 5)
             break;
-        p = p->next;
-        p = rfgstr(table[row][col], 31, p);
-        if (p != refal.nexta)
+        current_argument = current_argument->next;
+        current_argument = get_string_expression(table[row][column], 31, current_argument, refal.next_argument);
+        if (current_argument != refal.next_argument)
             break;
         return;
     } while (false);
@@ -166,26 +166,25 @@ void (*view_table_1)(void) = view_table_;
 // <View_Show> == 'Q' | 'B' | 'P' | 'N' | 'E'
 static void view_show_(void)
 {
-    if (refal.preva->next != refal.nexta)
+    if (refal.previous_argument->next != refal.next_argument)
     {
         refal.upshot = 2;
         return;
     }
-    T_LINKCB *p = refal.preva;
     const int result = view_Show(answer, steps, steps_Back, rule, rule_Text, table, position);
-    p->tag = TAGO;
-    p->info.codep = NULL;
+    refal.previous_argument->tag = TAGO;
+    refal.previous_argument->info.codep = NULL;
     if (result == 1)
-        p->info.infoc = 'B';
+        refal.previous_argument->info.infoc = 'B';
     else if (result == 2)
-        p->info.infoc = 'P';
+        refal.previous_argument->info.infoc = 'P';
     else if (result == 3)
-        p->info.infoc = 'N';
+        refal.previous_argument->info.infoc = 'N';
     else if (result == 4)
-        p->info.infoc = 'E';
+        refal.previous_argument->info.infoc = 'E';
     else
-        p->info.infoc = 'Q';
-    rftpl(refal.prevr, refal.nextr, refal.nexta);
+        refal.previous_argument->info.infoc = 'Q';
+    transplantation(refal.previous_result, refal.previous_argument->previous, refal.previous_argument->next);
     return;
 }
 char view_show_0[] = {Z1 'V', 'I', 'E', 'W', '_', 'S', 'H', 'O', 'W', (char)9};
@@ -195,16 +194,16 @@ void (*view_show_1)(void) = view_show_;
 // <Dialog_Is_Exit> == &True | &False
 static void dialog_is_exit_(void)
 {
-    if (refal.preva->next != refal.nexta)
+    if (refal.previous_argument->next != refal.next_argument)
     {
         refal.upshot = 2;
         return;
     }
     if (dialog_Is_Exit())
-        refal.preva->info.codef = &refalab_true;
+        refal.previous_argument->info.codef = &refalab_true;
     else
-        refal.preva->info.codef = &refalab_false;
-    rftpl(refal.prevr, refal.nextr, refal.nexta);
+        refal.previous_argument->info.codef = &refalab_false;
+    transplantation(refal.previous_result, refal.previous_argument->previous, refal.previous_argument->next);
     return;
 }
 char dialog_is_exit_0[] = {Z6 'D', 'I', 'A', 'L', 'O', 'G', '_', 'I', 'S', '_', 'E', 'X', 'I', 'T', (char)14};
